2950692364e58ed3e8dcdb2cef083ff260ea1e71
ansakharov <alesakharov@ozon.ru>
2022-10-27 00:06:49 +0300

    fix task

--- 2_maps/3_once_with_map/main.go
+++ 2_maps/3_once_with_map/main.go
@@ -17,6 +17,7 @@ func main() {
 	for i := 0; i < capacity; i++ {
 		doubles = append(doubles, rand.Intn(10)) // create rand num 0...9
 	}
+	// 3, 4, 5, 0, 4, 9, 8, 6, 6, 5, 5, 4, 4, 4, 2, 1, 2, 3, 1 ...
 
 	uniqueIDs := make(chan int, capacity)
 	wg := sync.WaitGroup{}
@@ -25,6 +26,7 @@ func main() {
 		i := i
 		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			if _, ok := alreadyStored[doubles[i]]; !ok {
 				mu.Lock()
 				alreadyStored[doubles[i]] = struct{}{}
